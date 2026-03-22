b7418dfefbc7801e9167a99100261aa69a7c0a6c
33vr3y <live.nolen@gmail.com>
2026-03-22 17:59:36 +0300

    lesson 7 JUNIT

---
+++ src/test/java/lesson_7_junit/TriangleTest.java
@@ -0,0 +1,13 @@
+package lesson_7_junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+    @Test
+    void testCalculateArea(){
+        Triangle triangle = new Triangle(2, 5, 4);
+        double result = triangle.calculateArea();
+        Assertions.assertEquals( 3.7996, result, 0.001);
+    }
+}
